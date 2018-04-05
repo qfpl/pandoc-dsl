081015a6f49e54acadddbc2432d386816a7f0a51
Tony Morris <tmorris@tmorris.net>
2018-04-05 14:35:27 +1000

    test

--- test/Tests.hs
+++ test/Tests.hs
@@ -10,6 +10,7 @@ import Papa
 import Test.Tasty(defaultMain, testGroup)
 import Test.Tasty.Hedgehog
 import Text.Pandoc.Definition
+import qualified Text.Pandoc.Definition as Definition
 
 main ::
   IO ()
@@ -20,23 +21,137 @@ main =
     ]
   ]
 
-prop_reverse :: Property
+prop_reverse ::
+  Property
 prop_reverse =
   property $ do
-    xs <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
+    xs <- forAll $ alpha100
     reverse (reverse xs) === xs
 
 undefined = undefined
 
+unicode100 ::
+  Gen String
+unicode100 =
+  Gen.list (Range.linear 0 100) Gen.unicode
+
+alpha100 ::
+  Gen String
+alpha100 =
+  Gen.list (Range.linear 0 100) Gen.alpha
+
+genQuoteType ::
+  Gen QuoteType
+genQuoteType =
+  Gen.element
+    [
+      SingleQuote
+    , DoubleQuote
+    ]
+
+
+genMathType ::
+  Gen MathType
+genMathType =
+  Gen.element
+    [
+      DisplayMath
+    , InlineMath
+    ]
+
+genTarget ::
+  Gen Target
+genTarget =
+  ((,) <$> unicode100 <*> unicode100)
+
+genAttr ::
+  Gen Attr
+genAttr =
+  do  it <- unicode100
+      cl <- Gen.list (Range.linear 0 100) (unicode100)
+      kv <- Gen.list (Range.linear 0 100) ((,) <$> unicode100 <*> unicode100)
+      pure (it, cl, kv)
+
+genFormat ::
+  Gen Format
+genFormat =
+  Definition.Format <$> unicode100 
+
+genInlines ::
+  Range Int
+  -> Gen [Inline]
+genInlines r =
+  Gen.list r genInline
+
 genInline ::
   Gen Inline
 genInline =
-  undefined
+  Gen.choice
+    [
+      Str <$> unicode100
+    , Emph <$> genInlines (Range.linear 0 100)
+    , Strong <$> genInlines (Range.linear 0 100)
+    , Strikeout <$> genInlines (Range.linear 0 100)
+    , Superscript <$> genInlines (Range.linear 0 100)
+    , Subscript <$> genInlines (Range.linear 0 100)
+    , SmallCaps <$> genInlines (Range.linear 0 100)
+    , Quoted <$> genQuoteType <*> genInlines (Range.linear 0 100)
+    , Cite <$> genCitations (Range.linear 0 100) <*> genInlines (Range.linear 0 100)
+    , Code <$> genAttr <*> unicode100
+    , pure Definition.Space
+    , pure SoftBreak
+    , pure LineBreak
+    , Math <$> genMathType <*> unicode100
+    , RawInline <$> genFormat <*> unicode100 
+    , Link <$> genAttr <*> genInlines (Range.linear 0 100) <*> genTarget 
+    , Image <$> genAttr <*> genInlines (Range.linear 0 100) <*> genTarget 
+    , Note <$> genBlocks (Range.linear 0 100)
+    , Span <$> genAttr <*> genInlines (Range.linear 0 100)
+    ]
    
+genBlocks ::
+  Range Int
+  -> Gen [Block]
+genBlocks r =
+  Gen.list r genBlock
+
 genBlock ::
   Gen Block
 genBlock =
   undefined
+
+genCitations ::
+  Range Int
+  -> Gen [Citation]
+genCitations r =
+  Gen.list r genCitation
+
+genCitation ::
+  Gen Citation
+genCitation =
+  Citation <$>
+    unicode100 <*>
+    genInlines (Range.linear 0 100) <*>
+    genInlines (Range.linear 0 100) <*>
+    genCitationMode <*>
+    Gen.int (Range.linear 0 100) <*>
+    Gen.int (Range.linear 0 100)
+
+genCitationMode ::
+  Gen CitationMode
+genCitationMode =
+  Gen.element
+    [
+      AuthorInText
+    , SuppressAuthor
+    , NormalCitation
+    ]
+
+genMetaValues ::
+  Range Int
+  -> Gen [MetaValue]
+genMetaValues r =
+  Gen.list r genMetaValue
 
 genMetaValue ::
   Gen MetaValue
@@ -44,11 +159,11 @@ genMetaValue =
   Gen.choice
     [
       MetaBool <$> Gen.bool
-    , MetaString <$> Gen.list (Range.linear 0 100) Gen.alpha
+    , MetaString <$> alpha100
     , MetaMap <$> genMapMetaValue
-    , MetaList <$> Gen.list (Range.linear 0 100) genMetaValue
-    , MetaInlines <$> Gen.list (Range.linear 0 100) genInline
-    , MetaBlocks <$> Gen.list (Range.linear 0 100) genBlock
+    , MetaList <$> genMetaValues (Range.linear 0 100)
+    , MetaInlines <$> genInlines (Range.linear 0 100)
+    , MetaBlocks <$> genBlocks (Range.linear 0 100)
     ]
 
 genMapMetaValue ::
@@ -64,7 +179,7 @@ genMetaPair =
                 (1, pure "title")
               , (1, pure "author")
               , (1, pure "date")
-              , (97, Gen.list (Range.linear 0 100) Gen.alpha)
+              , (97, alpha100)
               ]
       v <- genMetaValue
       pure (k, v)
