5a8c41d869b2955deac61277d28efea13a7eec4b
Isaac Elliott <isaace71295@gmail.com>
2018-04-12 09:50:42 +1000

    Latest attempt at nix

--- default.nix
+++ default.nix
@@ -9,12 +9,24 @@ let
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
+ 
+  overriddenHaskellPackages =
+    pkgs.haskellPackages.override {
+        overrides = self: super: {
+          ansi-terminal = self.ansi-terminal_0_7_1_1;
+          texmath = self.texmath_0_10_1_1;
+          skylighting = self.skylighting_0_6;
+          pandoc-types = self.pandoc-types_1_17_3_1;
+          doctemplates = self.doctemplates_0_2_1;
+          hslua = self.hslua_0_9_5_1;
+          pandoc = self.pandoc_2_1_1;
+        };
+      };
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  pandoc = haskellPackages.callPackage ./nix/pandoc.nix {};
 
-  drv = variant (haskellPackages.callPackage f {});
+  drv = variant (overriddenHaskellPackages.callPackage f {});
 
 in
 
